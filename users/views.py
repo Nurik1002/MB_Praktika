859ee708414a3b7437be8c44e50b42f41504c3d9
Yarashayev Nurgeldi <nyarashbayev97@gmail.com>
2024-03-28 14:11:34 +0300

    Update files

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
-# Create your views here.
+def homeView(request):
+    return  render(request, "home.html", {})
