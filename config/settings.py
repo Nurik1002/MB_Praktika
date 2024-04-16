cb9e7818d3868803fc68776d9650f58c88cb4720
Yarashayev Nurgeldi <nyarashbayev97@gmail.com>
2024-04-16 14:23:54 +0300

    Create message models

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'messages',
+    'mymessages',
+    
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser'
---
+++ mymessages/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+# from models import Consultation, Chat, PrivateChat, GroupChat, Message
+
+
+# admin.site.register(Consultation)
+# admin.site.register(Chat)
+# admin.site.register(PrivateChat)
+# admin.site.register(GroupChat)
+# admin.site.register(Message)
