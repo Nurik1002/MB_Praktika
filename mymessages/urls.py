886f467f7b1204c49d619758da5bb009c6885f36
Yarashayev Nurgeldi <nyarashbayev97@gmail.com>
2024-04-17 14:49:57 +0300

    Consultations

--- mymessages/urls.py
+++ mymessages/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import consultatoinFilterByDoctor, consultation
+from .views import (
+    consultatoinFilterByDoctor, 
+    consultation, 
+    createConsultation, 
+    myDoctors, 
+    consultationFilterByUser,
+    
+    )
 
 urlpatterns = [
-    path('<int:pk>/filterConsultations/', consultatoinFilterByDoctor, name='filterConsultations'),
+    path("<int:pk>/filter/consultations/by/user/", consultationFilterByUser, name="filterConsultationByUser"),
+    path('<int:pk>/doctors/', myDoctors, name='myDoctors'),
+    path('create/consultation/', createConsultation, name='createConsultation'),
+    path('<int:pk>/filter/consultations/by/doctor/', consultatoinFilterByDoctor, name='filterConsultations'),
     path('<int:pk>/consultations/', consultation, name='doctorConsultations'),
 ]
--- mymessages/views.py
+++ mymessages/views.py
@@ -28,6 +28,28 @@ def consultatoinFilterByDoctor(request, pk):
         return JsonResponse({"error": "Invalid request" }, status=400)
     
 
+@login_required(login_url='login')
+def consultationFilterByUser(request, pk):
+    if request.method == "GET":
+        user  = CustomUser.objects.get(id=pk)
+        if user:
+            consultations = Consultation.objects.filter(user=user)
+            consultation_data = [
+                {
+                "id" : constation.id,
+                "title" : constation.title,
+                "description" : constation.description,
+                "user_id" : constation.user_id,
+                "doctor_id" : constation.doctor_id
+                }
+                for constation in consultations
+            ]
+            return JsonResponse({"consultations": consultation_data}, status=200)
+        else:
+            return JsonResponse({"error": "Missing doctor id" }, status=400)
+    else:
+        return JsonResponse({"error": "Invalid request" }, status=400)
+    
 
 
 @login_required(login_url='login')
@@ -35,3 +57,12 @@ def consultation(request, pk):
     user = CustomUser.objects.get(id=pk)
     doctor = Doctor.objects.get(user=user)
     return render(request, "doctor/consultations.html", {"doctor": doctor})
+
+@login_required(login_url='login')
+def createConsultation(request):
+    return render(request, "patient/createConsultation.html")
+
+@login_required(login_url='login')
+def myDoctors(request, pk):
+    user = CustomUser.objects.get(id=pk)
+    return render(request, "patient/myConsultations.html", {"user": user})
