ade58f3d9304986d1b254873a7f266a072a65117
Yarashayev Nurgeldi <nyarashbayev97@gmail.com>
2024-05-26 13:24:36 +0300

    Image Filters

--- image_filters/views.py
+++ image_filters/views.py
@@ -1,42 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .forms import ImageUploadForm
-from .img_filters import gaussian_blur, unsharp_mask, median_blur, clahe_filter
+from .img_filters import gaussian_blur, unsharp_mask, median_blur, clahe_filter 
 from django.conf import settings
 from django.core.files.storage import default_storage
 import cv2
 import numpy as np
+import base64
 
 def img_filters(request):
-    if request.method == "POST":
-        form = ImageUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            image = form.cleaned_data["image"]
-            filename = image.name
-            image_path = settings.MEDIA_ROOT + '/' + filename
-            default_storage.save(filename, image)
+    if request.method == 'POST':
+        # Check if an image was actually selected
+        if 'image' in request.FILES:
+            # Get the uploaded image
+            image_file = request.FILES['image']
 
-            filter_name = request.POST.get('filter_name')  
+            # Save the uploaded image to temporary storage
+            image_path = default_storage.save('temp.jpg', image_file)
+            full_path = settings.MEDIA_ROOT + '/' + image_path
 
-            if filter_name == 'gaussian_blur':
-                processed_img = gaussian_blur(image_path)
-            elif filter_name == 'unsharp_mask':
-                processed_img = unsharp_mask(image_path)
-            elif filter_name == 'median_blur':
-                processed_img = median_blur(image_path)
-            elif filter_name == 'clahe':
-                processed_img = clahe_filter(image_path)  # Call the new filter function
-            else:
-                processed_img = cv2.imread(image_path)
+            # Apply the filter based on user selection
+            filter_type = request.POST.get('filter')
+            if filter_type == 'clahe':
+                processed_img = clahe_filter(full_path)
+            elif filter_type == 'gaussian':
+                processed_img = gaussian_blur(full_path)
+            elif filter_type == 'unsharp':
+                processed_img = unsharp_mask(full_path)
+            elif filter_type == 'median':
+                processed_img = median_blur(full_path)
+          
 
-            processed_filename = 'processed_' + filename
-            cv2.imwrite(settings.MEDIA_ROOT + '/' + processed_filename, processed_img)
+            _, buffer = cv2.imencode('.jpg', processed_img)
+            processed_image_data = base64.b64encode(buffer).decode('utf-8')
 
-            return JsonResponse({
-                'success': True,
-                'processed_image': processed_filename, 
-                'media_url': settings.MEDIA_URL 
-            })
+            
+            return render(request, 'image_filters/filters.html',   {'processed_image': processed_image_data,    'original_image_path': image_path})
+        else:
+            return render(request, 'image_filters/filters.html', {'error': 'Please select an image.'})
     else:
-        form = ImageUploadForm()
-    return render(request, 'image_filters/filters.html', {'form': form})
+        return render(request, 'image_filters/filters.html')
